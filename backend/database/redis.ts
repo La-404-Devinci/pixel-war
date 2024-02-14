35b135fa6549bde1021c62af52c3cf838c9c325e
Kan-A-Pesh <michelm.gaming@gmail.com>
2024-02-14 15:41:02 +0100

    🔧 Update .env.example and redis.ts files

--- backend/database/redis.ts
+++ backend/database/redis.ts
@@ -1,6 +1,17 @@
 import { createClient } from "redis";
 
-const getRedisClient = createClient()
+const user = process.env.REDIS_USER;
+const password = process.env.REDIS_PASSWORD;
+const credentials = user && password ? `${user}:${password}@` : "";
+
+const databaseNumber = process.env.REDIS_DATABASE;
+const database = databaseNumber ? `/${databaseNumber}` : "";
+
+const redisUrl = `redis://${credentials}${process.env.REDIS_HOST}:${process.env.REDIS_PORT}${database}`;
+
+const getRedisClient = createClient({
+    url: redisUrl,
+})
     .on("error", (err) => console.log("Redis Client Error", err))
     .connect();
 
