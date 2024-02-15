9857c1ecccbeb11c92c8a5ba2619e1a81eb45671
Michel Moccand-J <michelm.gaming@gmail.com>
2024-02-15 21:32:49 +0100

    🏗️ Move login import and rename to module

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 // import { useState } from 'react'
 import './App.css'
-import './styles/login.css'
 import LoginComponent from './pages/login'
 
 function App() {
--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import './styles/login.module.css';
 
 const LoginComponent = () => {
     
