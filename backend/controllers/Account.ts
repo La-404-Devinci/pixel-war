1a2c65a9cd86100f41fa75698532bbcb28372100
Alexi Reyes <alexireyes6796@gmail.com>
2024-02-26 20:07:39 +0100

    added ban-unban

--- backend/controllers/Account.ts
+++ backend/controllers/Account.ts
@@ -1,4 +1,13 @@
-import type express from "express";
+import type SocketIO from "socket.io";
+import express from "express";
+import nodemailer from "nodemailer";
+import {
+    generateAuthenticationToken,
+    generateAuthorizationToken,
+    verifyAuthorizationToken,
+} from "../auth/tokenUtils";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 class AccountController {
     /**
@@ -8,12 +17,16 @@ class AccountController {
      * @param req The Express request object
      * @param res The Express response object
      */
-    public static async sendMagicLink(req: express.Request, res: express.Response) {
+    public static async sendMagicLink(
+        req: express.Request,
+        res: express.Response
+    ) {
         // TODO: Send a magic link containing the AUTHORIZATION token to the user's email
         /**
          * VALIDATION
          * * Validate the user email (must be a Devinci email)
          *
+         
          * PROCESS
          * * Generate an AUTHORIZATION token
          * * Send the magic link to the user's email
@@ -22,6 +35,44 @@ class AccountController {
          * * Send a success message
          * * Send an error message if the email is invalid
          */
+        const { email } = req.body;
+
+        const isDevinciEmail = (email: string): boolean => {
+            const expression: RegExp = /^[a-zA-Z0-9._-]+@edu\.devinci.fr$/;
+
+            return expression.test(email);
+        };
+
+        if (isDevinciEmail(email) == true) {
+            const token: string = generateAuthorizationToken(email);
+            const link: string = `url/login?token=${token}`;
+
+            const transporter = nodemailer.createTransport({
+                host: "your_host",
+                port: 587,
+                secure: true,
+                auth: {
+                    user: "your_email_address",
+                    pass: "your_email_password",
+                },
+            });
+
+            const message = {
+                from: "your_email",
+                to: email,
+                subject: "Lien pour se connecter",
+                html: `Clique pour te connecter: <a href="${link}">${link}</a>`,
+            };
+
+            try {
+                await transporter.sendMail(message);
+                res.status(200).send("Lien envoyé. Regarder vos mails.");
+            } catch (error) {
+                res.status(500).send("Une erreur s'est produite.");
+            }
+        } else {
+            res.send("Email non valide");
+        }
     }
 
     /**
@@ -32,19 +83,30 @@ class AccountController {
      * @param res The Express response object
      */
     public static async login(req: express.Request, res: express.Response) {
-        // TODO: Log the user
-        /**
-         * VALIDATION
-         * * Validate AUTHORIZATION token
-         *
-         * PROCESS
-         * * Generate an AUTHENTICATION token
-         *
-         * RESPONSE
-         * * Send the AUTHENTICATION token
-         * * Send an error message if the AUTHORIZATION token is invalid
-         * * Send an error message if the AUTHORIZATION token is expired
-         */
+        const { token, email } = req.body;
+        if (!verifyAuthorizationToken(token, email)) {
+            return res.status(401).send("Invalid token");
+        }
+
+        try {
+            const user = await prisma.account.findFirst({
+                where: {
+                    devinciEmail: email,
+                },
+            });
+
+            if (!user) {
+                await prisma.account.create({
+                    data: {
+                        devinciEmail: email,
+                    },
+                });
+            }
+        } catch (error) {
+            return res.status(500).send("Unable to connect to the database");
+        }
+
+        return res.status(200).send(generateAuthenticationToken(email));
     }
 
     // Admin routes
@@ -92,6 +154,80 @@ class AccountController {
          * * Send a success message
          * * Send an error message if the user ID is invalid
          */
+        const { user, target } = req.body;
+
+        const isUserAdmin = (id: number): boolean => {
+            try {
+                const user = prisma.account.findUnique({
+                    where: { id: id },
+                    select: { isAdmin: true }
+                });
+          
+                return user?.isAdmin ?? false;
+            } catch (error) {
+                return false; 
+            }
+        }
+
+        const isUserBanned = (id: number): boolean => {
+            try {
+                const user = prisma.account.findUnique({
+                    where: { id: id },
+                    select: { isBanned: true }
+                });
+          
+                return user?.isAdmin ?? false;
+            } catch (error) {
+                console.log(error);
+                return false; 
+            }
+        }
+
+        if (isUserAdmin(user.id) == false) {
+            res.send("User is not an admin");
+            return; 
+        }
+          
+        
+        try {
+            if (isUserBanned(target.id) == true) {
+                await prisma.account.update({
+                    where: { id: target.id },
+                    data: { isBanned: false }
+                });
+    
+                res.send("User unbanned successfully");
+            } else if (isUserBanned(target.id) == false) {
+                await prisma.account.update({
+                    where: { id: target.id },
+                    data: { isBanned: true }
+                });
+            
+                res.send("User banned successfully");
+            }
+        } catch (error) {
+            console.error(error);
+            res.status(500).send("Error banning/unbanning user");
+            return;
+        }
+    }
+
+    /**
+     * Auth a websocket client
+     * @server WebSocket
+     *
+     * @param socket The client socket
+     * @param data The payload
+     */
+    public static async authSocket(socket: SocketIO.Socket, [token, email]: [string, string]) {
+        if (verifyAuthenticationToken(token, email)) {
+            socket.data.token = token;
+            socket.data.email = email;
+
+            socket.emit("auth-callback", true);
+        } else {
+            socket.emit("auth-callback", false);
+        }
     }
 }
 
