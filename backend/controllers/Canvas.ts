9556389c78dfcc5577c25ce4659e4a4af32f4d44
AnnamTRAN <annampierretran@gmail.com>
2024-02-29 21:43:45 +0100

    ♻️ - Refactor CSS styles and update dependencies

--- backend/controllers/Canvas.ts
+++ backend/controllers/Canvas.ts
@@ -35,7 +35,10 @@ class CanvasController {
      * @param req The Express request object
      * @param res The Express response object
      */
-    public static async getCanvasImage(req: express.Request, res: express.Response) {
+    public static async getCanvasImage(
+        req: express.Request,
+        res: express.Response
+    ) {
         res.status(200).json({
             pixels: this._canvas.pixels,
             width: this._canvas.width,
@@ -50,7 +53,15 @@ class CanvasController {
      * @param socket The socket that sent the pixel data
      * @param data The payload
      */
-    public static async placePixel(socket: SocketIO.Socket, [x, y, palette, callback]: [number, number, number, (timer: number) => void]) {
+    public static async placePixel(
+        socket: SocketIO.Socket,
+        [x, y, palette, callback]: [
+            number,
+            number,
+            number,
+            (timer: number) => void,
+        ]
+    ) {
         // Get the user
         const user = await prisma.account.findFirst({
             where: {
@@ -62,11 +73,25 @@ class CanvasController {
         if (user.isBanned) return callback(0);
 
         // Check if the user timer is elapsed
-        if (user.lastPixelTime && new Date(user.lastPixelTime).getTime() > new Date().getTime() - this._canvas.cooldown * 1000) {
-            return callback(new Date(user.lastPixelTime).getTime() - new Date().getTime() + this._canvas.cooldown * 1000);
+        if (
+            user.lastPixelTime &&
+            new Date(user.lastPixelTime).getTime() >
+                new Date().getTime() - this._canvas.cooldown * 1000
+        ) {
+            return callback(
+                new Date(user.lastPixelTime).getTime() -
+                    new Date().getTime() +
+                    this._canvas.cooldown * 1000
+            );
         }
 
-        if (x < 0 || y < 0 || x >= this._canvas.width || y >= this._canvas.height) return callback(0);
+        if (
+            x < 0 ||
+            y < 0 ||
+            x >= this._canvas.width ||
+            y >= this._canvas.height
+        )
+            return callback(0);
 
         // Get pixel index (from the canvas buffer)
         const pixelIndex = (y * this._canvas.width + x) * 3;
@@ -102,7 +127,10 @@ class CanvasController {
 
             await prisma.account.update({
                 where: { id: user.id },
-                data: { lastPixelTime: new Date(), placedPixels: user.placedPixels },
+                data: {
+                    lastPixelTime: new Date(),
+                    placedPixels: user.placedPixels,
+                },
             });
         }
 
@@ -120,7 +148,10 @@ class CanvasController {
      * @param req The Express request object
      * @param res The Express response object
      */
-    public static async resetCanvas(req: express.Request, res: express.Response) {
+    public static async resetCanvas(
+        req: express.Request,
+        res: express.Response
+    ) {
         prisma.logEntry.create({
             data: {
                 devinciEmail: "null",
@@ -147,7 +178,10 @@ class CanvasController {
      * @param req The Express request object
      * @param res The Express response object
      */
-    public static async changeCanvasSize(req: express.Request, res: express.Response) {
+    public static async changeCanvasSize(
+        req: express.Request,
+        res: express.Response
+    ) {
         const { height, width }: { height: number; width: number } = req.body;
 
         if (width < 0 || height < 0) {
@@ -185,11 +219,13 @@ class CanvasController {
      * @param req The Express request object
      * @param res The Express response object
      */
-    public static async changePixelPlacementCooldown(req: express.Request, res: express.Response) {
+    public static async changePixelPlacementCooldown(
+        req: express.Request,
+        res: express.Response
+    ) {
         // TODO: Change the pixel placement cooldown and log the action
         /**
          * VALIDATION
-         * * Check if the user is an admin
          * * Validate the new cooldown
          *
          * PROCESS
@@ -203,6 +239,9 @@ class CanvasController {
          * * Send the updated user data to all clients
          * * Send the updated canvas to all clients
          */
+        const { cooldown }: { cooldown: number } = req.body;
+
+        if (cooldown < 0) return res.status(400).send("Invalid cooldown");
     }
 
     /**
@@ -212,7 +251,10 @@ class CanvasController {
      * @param req The Express request object
      * @param res The Express response object
      */
-    public static async editCanvasColorPalette(req: express.Request, res: express.Response) {
+    public static async editCanvasColorPalette(
+        req: express.Request,
+        res: express.Response
+    ) {
         // TODO: Edit the canvas color palette and log the action
         /**
          * VALIDATION
