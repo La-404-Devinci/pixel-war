6a393b4b219871a7af5d9353cfe3aa8dff395049
AnnamTRAN <annampierretran@gmail.com>
2024-03-01 11:06:45 +0100

    ✨- Refactor AccountController and add setAssociation endpoint

--- backend/index.ts
+++ backend/index.ts
@@ -21,13 +21,20 @@ const server = http.createServer(app);
 WSS.init(server);
 
 WSS.io.on("connection", (socket: Socket) => {
-    const ip = socket.handshake.headers["x-forwarded-for"] || socket.handshake.address;
+    const ip =
+        socket.handshake.headers["x-forwarded-for"] || socket.handshake.address;
     const userAgent = socket.handshake.headers["user-agent"];
     console.log(`Socket connected from ${ip} using ${userAgent}`);
 
-    socket.on("auth", (...data) => AccountController.authSocket(socket, ...data));
-    socket.on("place-pixel", (...data) => CanvasController.placePixel(socket, ...data));
-    socket.on("message", (...data) => ChatController.broadcastMessage(socket, ...data));
+    socket.on("auth", (...data) =>
+        AccountController.authSocket(socket, ...data)
+    );
+    socket.on("place-pixel", (...data) =>
+        CanvasController.placePixel(socket, ...data)
+    );
+    socket.on("message", (...data) =>
+        ChatController.broadcastMessage(socket, ...data)
+    );
 
     WSS.updateClassement(socket);
 
@@ -40,6 +47,9 @@ WSS.io.on("connection", (socket: Socket) => {
 app.post("/auth/send-magic-link", AccountController.sendMagicLink);
 app.get("/auth/login", AccountController.login);
 app.get("/canvas/image", CanvasController.getCanvasImage);
+
+// Asso routes
+app.post("/api/asso", AccountController.setAssociation);
 
 // Admin routes
 const router = express.Router();
