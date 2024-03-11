c22ca1c5e500b8a76d1aff5cffbe27ecf0922eea
AnnamTRAN <annampierretran@gmail.com>
2024-03-11 07:31:21 +0100

    🚧 - Update AssoModal component to use AsyncSelect and add custom styling for options

--- frontend/src/components/AssoModal.tsx
+++ frontend/src/components/AssoModal.tsx
@@ -1,28 +1,86 @@
 // import React, { useEffect } from 'react'
 import { useState } from "react";
 import styles from "../styles/assoModal.module.css";
-import Select from "react-select";
+import AsyncSelect from "react-select/async";
+
 // import { JSX } from 'react/jsx-runtime';
 
 const options = [
-    { value: "asso1", label: "Association 1" },
-    { value: "asso2", label: "Association 2" },
-    { value: "asso3", label: "Association 3" },
+    {
+        image: "https://www.forum-associatif-numerique.fr/uploads/iimpact/logos/Logo-Monochrome-avec-NOUVELLE-ecriture-630fc58ce8a48.png",
+        label: "IIMPACT",
+        value: "iimpact",
+    },
+    {
+        image: "https://www.forum-associatif-numerique.fr/uploads/3v/logos/LogoFullColor-MaelleLemaignen-61191bd85b6f7.png",
+        label: "3V",
+        value: "3v",
+    },
+    {
+        image: "https://www.forum-associatif-numerique.fr/uploads/vincisquad/logos/1-Logo-Couleurs-631c4239386ae.png",
+        label: "Vinci Squad",
+        value: "vincisquad",
+    },
 ];
 
 export default function AssoModal() {
-    const [selectedOption, setSelectedOption] = useState<{
-        value: string;
-        label: string;
-    } | null>(null);
-
     const [displayAssoModal, setDisplayAssoModal] = useState(true);
+    const [selectedAsso, setSelectedAsso] = useState(null);
 
     const handleHide = (
         event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
     ) => {
         event.preventDefault();
         setDisplayAssoModal(false);
+    };
+
+    type CustomMenuProps = {
+        children: React.ReactNode;
+    };
+
+    const customMenu = (props: CustomMenuProps) => {
+        return (
+            <div className={styles.menu}>
+                <div>{props.children}</div>
+            </div>
+        );
+    };
+
+    type Option = {
+        image: string;
+        label: string;
+        value: string;
+    };
+
+    const filterAsso = (inputValue: string) => {
+        return options.filter((i: Option) =>
+            i.label.toLowerCase().includes(inputValue.toLowerCase())
+        );
+    };
+
+    const loadOptions = (
+        inputValue: string,
+        callback: (options: Option[]) => void
+    ) => {
+        setTimeout(() => {
+            callback(filterAsso(inputValue));
+        }, 1000);
+    };
+
+    type CustomOptionProps = {
+        data: Option;
+    };
+
+    const customOption = (props: CustomOptionProps) => {
+        return (
+            <div className={styles.option}>
+                <p>{props.data.label}</p>
+                <img
+                    src={props.data.image}
+                    alt={props.data.label}
+                />
+            </div>
+        );
     };
 
     return (
@@ -38,10 +96,14 @@ export default function AssoModal() {
                                 la juste ici pour la représenter.
                             </p>
 
-                            <Select
-                                defaultValue={selectedOption}
-                                onChange={setSelectedOption}
-                                options={options}
+                            <AsyncSelect
+                                placeholder='Choisissez votre association !'
+                                loadOptions={loadOptions}
+                                defaultOptions
+                                components={{
+                                    Menu: customMenu,
+                                    Option: customOption,
+                                }}
                             />
 
                             <button>C'est parti !</button>
