6efdb45db4bafddd61d43abefea5b53ae7264be8
Kan-A-Pesh <michelm.gaming@gmail.com>
2024-03-27 19:18:23 +0100

    🚑 Refactor broadcastMessage method in ChatController

--- backend/controllers/Chat.ts
+++ backend/controllers/Chat.ts
@@ -22,100 +22,104 @@ class ChatController {
      * @param data The payload
      */
     public static async broadcastMessage(socket: SocketIO.Socket, [message, callback]: [string, (success: boolean) => void]) {
-        if (!message || message.length < 1 || message.length > 200) {
-            console.log("Message is empty or too long");
-            callback(false);
-            return;
-        }
+        try {
+            if (!message || message.length < 1 || message.length > 200) {
+                console.log("Message is empty or too long");
+                callback(false);
+                return;
+            }
 
-        // Check if the message contains bad words
-        const cleanMessage = leoProfanity.clean(message);
+            // Check if the message contains bad words
+            const cleanMessage = leoProfanity.clean(message);
 
-        if (!socket.data.email) {
-            console.log("User is not authenticated");
-            callback(false);
-            return;
-        }
+            if (!socket.data.email) {
+                console.log("User is not authenticated");
+                callback(false);
+                return;
+            }
 
-        const user = await prisma.account.findFirst({
-            where: {
-                devinciEmail: socket.data.email,
-            },
-        });
+            const user = await prisma.account.findFirst({
+                where: {
+                    devinciEmail: socket.data.email,
+                },
+            });
 
-        if (!user) {
-            console.log("User not found");
-            callback(false);
-            return;
-        }
+            if (!user) {
+                console.log("User not found");
+                callback(false);
+                return;
+            }
 
-        if (user.isMuted) {
-            console.log("User is muted");
-            callback(false);
-            return;
-        }
+            if (user.isMuted) {
+                console.log("User is muted");
+                callback(false);
+                return;
+            }
 
-        const now = new Date();
+            const now = new Date();
 
-        const lastTimestamps = ((user.lastSentMessageTimes as number[]) ?? []).filter((timestamp) => timestamp > now.getTime() - 5000);
-        if (lastTimestamps.length > 4) {
-            user.isMuted = true;
+            const lastTimestamps = ((user.lastSentMessageTimes as number[]) ?? []).filter((timestamp) => timestamp > now.getTime() - 5000);
+            if (lastTimestamps.length > 4) {
+                user.isMuted = true;
+                // Save the user
+                await prisma.account.update({
+                    where: { id: user.id },
+                    data: { isMuted: true },
+                });
+
+                callback(false);
+                return;
+            }
+
+            user.lastSentMessageTimes = [...lastTimestamps, now.getTime()];
+
             // Save the user
             await prisma.account.update({
                 where: { id: user.id },
-                data: { isMuted: true },
+                data: { lastSentMessageTimes: user.lastSentMessageTimes, messagesSent: user.messagesSent + 1 },
             });
 
-            callback(false);
-            return;
-        }
-
-        user.lastSentMessageTimes = [...lastTimestamps, now.getTime()];
-
-        // Save the user
-        await prisma.account.update({
-            where: { id: user.id },
-            data: { lastSentMessageTimes: user.lastSentMessageTimes, messagesSent: user.messagesSent + 1 },
-        });
-
-        await prisma.logEntry.create({
-            data: {
-                devinciEmail: user.devinciEmail,
-                time: new Date().getTime(),
-                ip: socket.handshake.address,
-                action: {
-                    type: "message",
-                    originalContent: message,
-                    content: cleanMessage,
+            await prisma.logEntry.create({
+                data: {
+                    devinciEmail: user.devinciEmail,
+                    time: new Date().getTime(),
+                    ip: socket.handshake.address,
+                    action: {
+                        type: "message",
+                        originalContent: message,
+                        content: cleanMessage,
+                    },
                 },
-            },
-        });
-
-        // Add the message to the history
-        ChatController._messageHistory.push([user.devinciEmail, cleanMessage]);
-        if (ChatController._messageHistory.length > 30) ChatController._messageHistory.shift();
-
-        WSS.broadcastMessage(user.devinciEmail, cleanMessage);
-        WSS.updateUserData(socket, user);
-
-        // Send message to discord (using the webhook)
-        try {
-            await fetch(process.env.DISCORD_WEBHOOK_URL as string, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    avatar_url: `https://ui-avatars.com/api/?name=${user.devinciEmail}&background=0D8ABC&color=fff`,
-                    username: user.devinciEmail,
-                    content: cleanMessage,
-                }),
             });
+
+            // Add the message to the history
+            ChatController._messageHistory.push([user.devinciEmail, cleanMessage]);
+            if (ChatController._messageHistory.length > 30) ChatController._messageHistory.shift();
+
+            WSS.broadcastMessage(user.devinciEmail, cleanMessage);
+            WSS.updateUserData(socket, user);
+
+            // Send message to discord (using the webhook)
+            try {
+                await fetch(process.env.DISCORD_WEBHOOK_URL as string, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        avatar_url: `https://ui-avatars.com/api/?name=${user.devinciEmail}&background=0D8ABC&color=fff`,
+                        username: user.devinciEmail,
+                        content: cleanMessage,
+                    }),
+                });
+            } catch (error) {
+                console.error("Error sending message to Discord", error);
+            }
+
+            callback(true);
         } catch (error) {
-            console.error("Error sending message to Discord", error);
+            console.error("Error broadcasting message", error);
         }
-
-        callback(true);
     }
 
     /**
